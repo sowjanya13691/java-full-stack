f7c32389c81255da94d120721adeaf009339f66b
Sowjanya Kommireddy <sowjanya13691@gmail.com>
2026-03-12 07:03:29 +0530

    day 6 completed

---
+++ day5/async.js
@@ -0,0 +1,11 @@
+async function greet() {
+return 'Hello!';
+}
+// Same as:
+function greet() {
+return Promise.resolve('Hello!');
+}
+// Both are consumed the same way:
+greet().then(msg => console.log(msg)); // 'Hello!'
+
+ 
